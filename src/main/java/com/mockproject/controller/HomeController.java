e3fa8b24a2387a07fe9aef671ed66bf6707678a8
DangKhoa97102 <dangkhoa97.102@gmail.com>
2022-03-28 12:23:26 +0700

    Login Part2

--- src/main/java/com/mockproject/controller/HomeController.java
+++ src/main/java/com/mockproject/controller/HomeController.java
@@ -2,22 +2,31 @@ package com.mockproject.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mockproject.entity.Products;
+import com.mockproject.entity.Users;
 import com.mockproject.service.ProductsService;
+import com.mockproject.service.UsersService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-	
+
 	@Autowired
 	private ProductsService productService;
-	
+
+	@Autowired
+	private UsersService usersService;
+
 	// localhost:8080/index
 	@GetMapping("index")
 	public String doGetIndex(Model model) {
@@ -25,9 +34,22 @@ public class HomeController {
 		model.addAttribute("products", products);
 		return "user/index";
 	}
-	
+
 	@GetMapping("login")
-	public String doGetLogin() {
-			return "user/login";
+	public String doGetLogin(Model model) {
+		model.addAttribute("userRequest", new Users());
+		return "user/login";
+	}
+
+	// forward != redirect ( chuyển hướng và chuyển tiếp )
+	@PostMapping("login")
+	public String doPostLogin(@ModelAttribute("userRequest") Users userRequest, HttpSession session) {
+		Users userResponse = usersService.doLogin(userRequest.getUsername(), userRequest.getHashPassword());
+		if(userResponse != null) {
+			session.setAttribute("currentUser", userResponse);
+			return "redirect:/index";
+		} else {
+			return "redirect:/login";
+		}
 	}
 }
